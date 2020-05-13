ab033e81ac91f2ecbd767bdf19326a47eacab0a9
Nimo Beeren <nimo@deity.io>
2020-05-13 16:54:15 +0200

    improve track search algorithm

--- src/util.ts
+++ src/util.ts
@@ -18,7 +18,24 @@ export async function safeRequest<T extends (...args: any) => Promise<any>>(
 }
 
 export function isCorrectTrack(candidate: string, target: string): boolean {
-  const threshold = 0.9;
-  const { similarity } = compareStrings(candidate, target) || {};
-  return similarity && similarity > threshold;
+  const threshold = 0.8;
+
+  let result = compareStrings(candidate, target) || {};
+  if (result?.similarity > threshold) {
+    return true;
+  }
+
+  target = target.replace(/\((feat|ft).*\)/i, "").trim();
+  result = compareStrings(candidate, target) || {};
+  if (result?.similarity > threshold) {
+    return true;
+  }
+
+  candidate = candidate.replace(/- \w+ mix/i, "").trim();
+  result = compareStrings(candidate, target) || {};
+  if (result?.similarity > threshold) {
+    return true;
+  }
+
+  return false;
 }
